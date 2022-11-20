e760c98ad8bbebc9defb0fec8b40932a00f7dd7c
pooi_ub_home <polischykpogi@gmail.com>
2022-11-20 18:11:20 +0300

    Initial commit

---
+++ src/CoreTestCase.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+//тут учился переменным класса и  внешним фукциям через применение методов класса this.OutFunc
+public class CoreTestCase {
+    int a= 11;
+    int b = 12;
+
+    @Test
+    public void MyFirstTest() {
+        int a = this.OutFunc(12);
+        System.out.println(a);
+        int b = this.OutFunc( 11,17);
+        System.out.println(b);
+    }
+    public int OutFunc(int number)
+    {
+        return number*2;
+    }
+    public int OutFunc(int number, int multiplaer)
+    {
+        return number + multiplaer;
+    }
+}
